386aeae94c39b5fe80052070b9a1d2adc252c1a9
feizhen <sysulz19920512@163.com>
2015-09-29 00:59:30 +0800

    initial 2.0

--- README.md
+++
@@ -1,3 +0,0 @@
-# MyAchievements
-
-MyAchievements 在线作业发布、提交、管理、评分系统
--- client/templates/admin/admin.js
+++
@@ -1,9 +0,0 @@
-Template.admin.onRendered(function(){
-    this.$(document).ready(function(){
-        $('ul.tabs').tabs();
-    });
-    this.$(document).ready(function() {
-        $('select').material_select();
-    });
-});
-
